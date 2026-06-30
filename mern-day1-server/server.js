cb3854a8a6fc907612b7cbe48fb49bb93a718b15
Aayush Basnet <aayush9823@gmail.com>
2026-06-30 13:42:18 +0545

    install mongoose and connect

--- mern-day1-server/server.js
+++ mern-day1-server/server.js
@@ -1,15 +1,16 @@
 import express from "express"
+import connectDB from "./db.js";
 
 const app = express();
 const PORT = 5000;
-
-app.use(express.json());
 
 const handleMiddleware = (req, res, next) => {
     console.log("Middleware is working");
     next();
 }
 
+//middleware
+app.use(express.json());
 app.use(handleMiddleware);
 
 app.get('/', (req, res) => {
@@ -17,16 +18,21 @@ app.get('/', (req, res) => {
 }
 );
 
+//params
 app.get('/GET/users/:id', (req, res) => {
     console.log(req.params);
     res.send("User received")
 });
 
+//query
 app.get('/GET/search', (req, res) => {
     console.log(req.query);
     res.send(`Receive query is ${req.query.term}`);
 }
 );
+
+//database function
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
